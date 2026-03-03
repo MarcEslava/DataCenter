834c636897e6db0b427676d5f0f7072b15abaa0c
Marc Eslava <91909702+MarcEslava@users.noreply.github.com>
2026-03-03 09:35:06 +0100

    shcedule dynamic

--- dags/ecoVital_ETL.py
+++ dags/ecoVital_ETL.py
@@ -55,7 +55,7 @@ dag = DAG(
     'ecovital_etl',
     default_args=default_args,
     description='EcoVital Orders ETL Pipeline',
-    schedule='30 11 * * *',
+    schedule=Variable.get("ecovital_schedule", default_var="30 11 * * *"),
     start_date=datetime(2026, 1, 1),
     catchup=False,
 )
